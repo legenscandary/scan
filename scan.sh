35663f7d891073c5dde8282fc35336723d55cff5
Ingo Breßler <dev@ingobressler.net>
2019-03-05 07:36:03 +0100

    installing latest scanbd 1.5.1 instead 1.4.4 from repo

--- scan.sh
+++ scan.sh
@@ -560,14 +560,30 @@ installPackages() {
     echo
     echo " => Installing additional software packages for image processing and file server:"
     echo
-    sudo apt install -y curl scanbd samba lockfile-progs imagemagick \
-        zbar-tools poppler-utils libtiff-tools scantailor
+    sudo apt install -y curl samba lockfile-progs imagemagick \
+        zbar-tools poppler-utils libtiff-tools scantailor sane-utils openbsd-inetd
     # get missing keys required for backports directly, dirmngr DNS is broken in this ver
     curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x7638D0442B90D010' \
         | sudo apt-key add -
     codename="$(lsb_release -cs)"
     sudo sh -c "echo 'deb https://deb.debian.org/debian ${codename}-backports main contrib non-free' > /etc/apt/sources.list.d/debian-backports.list"
     sudo apt-get update -y
+    # install latest scanbd 1.5.1
+    inetfn=/etc/inetd.conf
+    entry="$(grep 'sane-port.*saned$' $inetfn)"
+    sudo update-inetd --remove sane-port
+    tempdir="$(mktemp -d)"
+    pushd "$tempdir"
+    curl -O http://ftp.debian.org/debian/pool/main/c/confuse/libconfuse-common_3.2.2+dfsg-1_all.deb
+    curl -O http://ftp.debian.org/debian/pool/main/c/confuse/libconfuse2_3.2.2+dfsg-1_armhf.deb
+    curl -O http://ftp.debian.org/debian/pool/main/s/scanbd/scanbd_1.5.1-4_armhf.deb
+    echo 'f1423d3de46e57df6b7b300972571d3b4edf18e7befcdbebb422388eea91086b *libconfuse-common_3.2.2+dfsg-1_all.deb
+b47a9c2339bcd0599b1328971661f58fca5a4b86014a17e31f458add64c71a38 *libconfuse2_3.2.2+dfsg-1_armhf.deb
+1fa024fa18243196227c963245395e1c321d4d6f14f4a4235fffffeb76c73339 *scanbd_1.5.1-4_armhf.deb' | sha256sum --strict -c && sudo dpkg --force-confdef -i *.deb
+    popd
+    sudo sh -c "(echo '$entry'; echo) >> '$inetfn'"
+    sudo service scanbd restart
+
     echo
     echo " => Installing selected OCR packages:"
     echo
