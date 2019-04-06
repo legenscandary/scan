bf09eb2bbca95da1f806dc897be465ac0049c889
Ingo Breßler <dev@ingobressler.net>
2019-04-06 13:54:51 +0000

    install: inetd not needed
    - following Scanner_Button_Daemon article on wiki.archlinux.org

--- install+update.sh
+++ install+update.sh
@@ -71,7 +71,7 @@ installPackages()
     echo " => Installing additional software packages for image processing and file server:"
     echo
     sudo apt-get install -y git curl samba lockfile-progs imagemagick \
-        zbar-tools poppler-utils libtiff-tools scantailor sane-utils openbsd-inetd
+        zbar-tools poppler-utils libtiff-tools scantailor sane-utils
     # get missing keys required for backports directly, dirmngr DNS is broken in this ver
     curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x7638D0442B90D010' \
         | sudo apt-key add -
@@ -79,9 +79,6 @@ installPackages()
     sudo sh -c "echo 'deb https://deb.debian.org/debian ${codename}-backports main contrib non-free' > /etc/apt/sources.list.d/debian-backports.list"
     sudo apt-get update -y
     # install latest scanbd 1.5.1
-    inetfn=/etc/inetd.conf
-    entry="$(grep 'sane-port.*saned$' $inetfn)"
-    sudo update-inetd --remove sane-port
     tempdir="$(mktemp -d)"
     if cd "$tempdir"; then
         curl -O http://ftp.debian.org/debian/pool/main/c/confuse/libconfuse-common_3.2.2+dfsg-1_all.deb
@@ -91,8 +88,6 @@ installPackages()
 b47a9c2339bcd0599b1328971661f58fca5a4b86014a17e31f458add64c71a38 *libconfuse2_3.2.2+dfsg-1_armhf.deb
 1fa024fa18243196227c963245395e1c321d4d6f14f4a4235fffffeb76c73339 *scanbd_1.5.1-4_armhf.deb' | sha256sum --strict -c && sudo dpkg --force-confdef -i ./*.deb
     fi
-    sudo sh -c "(echo '$entry'; echo) >> '$inetfn'"
-    sudo service scanbd restart
 
     echo
     echo " => Installing selected OCR packages:"
