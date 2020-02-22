b7086918999fc9a8cbc33dcaeb999374f85cbc38
Ingo Breßler <dev@ingobressler.net>
2020-02-22 19:57:15 +0100

    installPackages() fixed for current raspbian buster

--- install+update.sh
+++ install+update.sh
@@ -76,36 +76,20 @@ installPackages()
     echo " => Installing additional software packages for image processing and file server:"
     echo
     sudo apt-get install -y git curl samba lockfile-progs imagemagick \
-        zbar-tools poppler-utils libtiff-tools scantailor sane-utils openbsd-inetd
+        zbar-tools poppler-utils libtiff-tools scantailor sane-utils openbsd-inetd \
+		libconfuse-common libconfuse2 scanbd
     # disable conflicting inetd config
     sudo update-inetd --disable sane-port # inetd not needed, all handled by systemd
     sudo service inetd restart
 
-    # install latest scanbd 1.5.1
-    tempdir="$(mktemp -d)"
-    if cd "$tempdir"; then
-        export DEBIAN_FRONTEND=noninteractive
-        curl -O http://ftp.debian.org/debian/pool/main/c/confuse/libconfuse-common_3.2.2+dfsg-1_all.deb
-        curl -O http://ftp.debian.org/debian/pool/main/c/confuse/libconfuse2_3.2.2+dfsg-1_armhf.deb
-        curl -O http://ftp.debian.org/debian/pool/main/s/scanbd/scanbd_1.5.1-4_armhf.deb
-        echo 'f1423d3de46e57df6b7b300972571d3b4edf18e7befcdbebb422388eea91086b *libconfuse-common_3.2.2+dfsg-1_all.deb
-b47a9c2339bcd0599b1328971661f58fca5a4b86014a17e31f458add64c71a38 *libconfuse2_3.2.2+dfsg-1_armhf.deb
-1fa024fa18243196227c963245395e1c321d4d6f14f4a4235fffffeb76c73339 *scanbd_1.5.1-4_armhf.deb' | sha256sum --strict -c && sudo dpkg --force-confdef -i ./*.deb
-    fi
 
-    # get missing keys required for backports directly, dirmngr DNS is broken in this ver
-    curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x7638D0442B90D010' \
-        | sudo apt-key add -
-    codename="$(lsb_release -cs)"
-    sudo sh -c "echo 'deb https://deb.debian.org/debian ${codename}-backports main contrib non-free' > /etc/apt/sources.list.d/debian-backports.list"
-    sudo apt-get update -y
 
     echo
     echo " => Installing selected OCR packages:"
     echo
     [ -z "$DOC_LANG" ] && DOC_LANG="eng" # minimal language, updated from config file later
     tess_lang_packs="$(IFS='+'; for l in $DOC_LANG; do echo tesseract-ocr-$l; done)"
-    sudo apt-get install -y -t "${codename}-backports" tesseract-ocr $tess_lang_packs
+    sudo apt-get install -y tesseract-ocr $tess_lang_packs
 }
 
 cfg()
